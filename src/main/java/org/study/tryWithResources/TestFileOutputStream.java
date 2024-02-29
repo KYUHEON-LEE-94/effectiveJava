762e2a430cfb9175b392007daab4a80c212a9e37
khlee <panpan68@naver.com>
2024-02-29 16:17:57 +0900

    ITEM9. try-finally 보다는 try-with-resources를 사용하라.

---
+++ src/main/java/org/study/tryWithResources/TestFileOutputStream.java
@@ -0,0 +1,26 @@
+package org.study.tryWithResources;
+
+import java.io.IOException;
+
+/**
+ * packageName    : org.study.tryWithResources
+ * fileName       : TestFileOutputStream
+ * author         : heon
+ * date           : 2024-02-29
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-02-29           heon               최초 생성
+ */
+public class TestFileOutputStream {
+    public void write(String writeData, boolean throwException) throws IOException {
+        System.out.println("WRITING .... : " + writeData);
+        if (throwException)
+            throw new IOException("[TestFileOutputStream readLine method] EXCEPTION !");
+    }
+
+    public void close() throws IOException {
+        throw new IOException("[TestFileOutputStream close method] EXCEPTION");
+    }
+}
