e7fe13cfcc6cdf36863e0a04f2320a70808b23ec
khlee <panpan68@naver.com>
2024-03-11 19:14:07 +0900

    ITEM16. public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용하라

---
+++ src/main/java/org/study/chapter3/ITEM16/OuterClass.java
@@ -0,0 +1,35 @@
+package org.study.chapter3.ITEM16;
+
+/**
+ * packageName    : org.study.chapter3.ITEM16
+ * fileName       : OuterClass
+ * author         : heon
+ * date           : 2024-03-11
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-03-11           heon               최초 생성
+ */
+public class OuterClass {
+    private InnerClass innerClass;
+
+    public OuterClass() {
+        this.innerClass = new InnerClass();
+    }
+
+    // 내부 클래스의 수정 기능을 제공하기 위한 퍼블릭 인터페이스
+    public void changeValue(int value) {
+        innerClass.value = value;
+    }
+
+    // 접근자(getter)
+    public int value() {
+        return innerClass.value;
+    }
+
+    // 내부 클래스
+    class InnerClass {
+        public int value;
+    }
+}
