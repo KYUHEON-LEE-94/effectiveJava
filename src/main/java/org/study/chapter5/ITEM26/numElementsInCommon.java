d32aa8d7ef22ef4d9721d6f25f6aa596a46563c6
khlee <panpan68@naver.com>
2024-03-22 14:35:48 +0900

    ITEM26. 로 타입(raw type)은 사용하지 말라

---
+++ src/main/java/org/study/chapter5/ITEM26/numElementsInCommon.java
@@ -0,0 +1,22 @@
+package org.study.chapter5.ITEM26;
+
+import java.util.Set;
+
+/**
+ * packageName    : org.study.chapter5.ITEM26
+ * fileName       : numElementsInCommon
+ * author         : heon
+ * date           : 2024-03-22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-03-22           heon               최초 생성
+ */
+public class numElementsInCommon {
+    public void printBook(Iterable<?> box) {
+        for (Object book : box) {
+            System.out.println(book);
+        }
+    }
+}
