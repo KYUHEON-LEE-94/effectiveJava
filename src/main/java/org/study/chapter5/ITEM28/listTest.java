b0bb6f2b0b21e4e39474d0ba0409540828cc9fb8
khlee <panpan68@naver.com>
2024-03-26 09:47:55 +0900

    ITEM 28. 배열보다는 리스트를 사용하라

---
+++ src/main/java/org/study/chapter5/ITEM28/listTest.java
@@ -0,0 +1,30 @@
+package org.study.chapter5.ITEM28;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * packageName    : org.study.chapter5.ITEM28
+ * fileName       : listTetst
+ * author         : heon
+ * date           : 2024-03-26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-03-26           heon               최초 생성
+ */
+public class listTest {
+    public static void main(String[] args) {
+        String[] strings = new String[3];
+        strings[0] = "1";
+        strings[1] = "2";
+        strings[2] = "3";
+
+        var StringList = new ArrayList<String>();
+        StringList.add("1");
+        StringList.add("2");
+        StringList.add("3");
+
+    }
+}
