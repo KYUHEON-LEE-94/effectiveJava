0d74eaae53226b9a40d90a3cb072b5ed830cd6d3
Maizurzu <panpan68@naver.com>
2024-05-18 09:16:50 +0900

    ITEM 61. 박싱된 기본 타입보다는 기본 타입을 사용하라

---
+++ src/test/java/org/study/chapter9/ITEM61/NoUseBoxingType.java
@@ -0,0 +1,56 @@
+package org.study.chapter9.ITEM61;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ * @author : heon
+ * @Description : AvoidFloatAndDouble.java
+ * @since : 2024-05-17
+ *
+ * <pre>
+ *
+ * << 개정이력(Modification Information) >>
+ *
+ * ===========================================================
+ * 수정인          수정자      수정내용
+ * ----------    ----------    --------------------
+ * 2024-05-17       heon         최초 생성
+ *
+ * <pre>
+ */
+public class NoUseBoxingType {
+
+    @Test
+    void 예상한_결과가_아니다(){
+        Comparator<Integer> naturalOrder = (i, j) -> (i < j) ? -1: (i == j ? 0 : 1);
+        int compare = naturalOrder.compare(Integer.valueOf(42), Integer.valueOf(42));
+        assertEquals(compare, 0);
+    }
+
+    @Test
+    void 언박싱_비교(){
+        Comparator<Integer> naturalOrder = (boxedI ,boxedJ) -> {
+            int i = boxedI, j = boxedJ;
+            return (i < j) ? -1 : (i == j) ? 0 : -1;
+        };
+    }
+
+    @Test
+    void Long과_long(){
+        long startTime = System.nanoTime();
+//        Long sum = 0L;
+        long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += 1;
+        }
+        long endTime = System.nanoTime();
+        double elapsedTimeSeconds = (endTime - startTime) / (double) 1_000_000_000;
+
+        System.out.println("how long did it take? : " + elapsedTimeSeconds + " seconds");
+    }
+}
