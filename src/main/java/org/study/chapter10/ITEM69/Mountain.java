9ad1022501def292b48454d1dbff5a9ba8900edc
khlee <panpan68@naver.com>
2024-05-29 08:52:29 +0900

    ITEM 69. 예외는 진짜 예외 상황에만 사용하라.

---
+++ src/main/java/org/study/chapter10/ITEM69/Mountain.java
@@ -0,0 +1,24 @@
+package org.study.chapter10.ITEM69;
+
+/**
+ * @author : heon
+ * @Description : Mountain.java
+ * @since : 2024-05-29
+ *
+ * <pre>
+ *
+ * << 개정이력(Modification Information) >>
+ *
+ * ===========================================================
+ * 수정인          수정자      수정내용
+ * ----------    ----------    --------------------
+ * 2024-05-29       heon         최초 생성
+ *
+ * <pre>
+ */
+public class Mountain {
+    public void climb() throws InterruptedException {
+        Thread.sleep(1000L);
+        System.out.println("한걸음~");
+    }
+}
---
+++ src/test/java/org/study/chapter10/ITEM69/MountainTest.java
@@ -0,0 +1,36 @@
+package org.study.chapter10.ITEM69;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author : heon
+ * @Description : MountainTest.java
+ * @since : 2024-05-29
+ *
+ * <pre>
+ *
+ * << 개정이력(Modification Information) >>
+ *
+ * ===========================================================
+ * 수정인          수정자      수정내용
+ * ----------    ----------    --------------------
+ * 2024-05-29       heon         최초 생성
+ *
+ * <pre>
+ */
+class MountainTest {
+
+    @Test
+    void climb() throws InterruptedException {
+        List<Mountain> range = Arrays.asList(new Mountain(), new Mountain(), new Mountain());
+        for (Mountain m : range) {
+            m.climb();
+        }
+    }
+
+}
