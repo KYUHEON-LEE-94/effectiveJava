b2afc56b3c33c4e35e740d016a3756c6d2f8b547
khlee <panpan68@naver.com>
2024-06-04 13:06:48 +0900

    ITEM 73. 추상화 수준에 맞는 예외를 던지라

---
+++ src/test/java/org/study/chapter10/ITEM73/ExceptionTest.java
@@ -0,0 +1,41 @@
+package org.study.chapter10.ITEM73;
+
+import org.junit.jupiter.api.Test;
+import org.study.chapter10.ITEM69.Mountain;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author : heon
+ * @Description : Test.java
+ * @since : 2024-06-04
+ *
+ * <pre>
+ *
+ * << 개정이력(Modification Information) >>
+ *
+ * ===========================================================
+ * 수정인          수정자      수정내용
+ * ----------    ----------    --------------------
+ * 2024-06-04       heon         최초 생성
+ *
+ * <pre>
+ */
+public class ExceptionTest {
+    @Test
+    void climb() throws InterruptedException {
+        try {
+            // 예외 생성
+            NumberFormatException ex = new NumberFormatException("Exception");
+
+            ex.initCause(new NullPointerException("근본 원인"));
+            throw ex;
+        } catch (NumberFormatException ex) {
+            ex.getCause().printStackTrace();
+        }
+
+        //checked exception ->unchecked exception
+        throw new RuntimeException(new Exception("런타임 예외로 변경"));
+    }
+}
